b1427a5c8032a64f78245a805f04f2fde414c0d9
alexandre deiness <alex-1602@hotmail.fr>
2016-09-14 16:20:12 +0200

    Update: $_POST sur register.php + inclusions des fonctions isUsernameAvailable() et isEmailAvailable()

--- register.php
+++ register.php
@@ -6,6 +6,19 @@ require('model/functions.fn.php');
 	Register
 ===============================*/
 
-include 'view/_header.php';
+if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])){
+	if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
+
+		header('Location: login.php');
+
+	}else{
+		$error = 'Champs requis !';
+	}
+}
+
+// username and email available
+isUsernameAvailable();
+isEmailAvailable();
+
 include 'view/register.php';
 include 'view/_footer.php';
