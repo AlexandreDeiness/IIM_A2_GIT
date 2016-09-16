8ebc20a534ecf1ff3e1cea849f75d7b37b6d3e23
alexandre deiness <alex-1602@hotmail.fr>
2016-09-16 15:21:28 +0200

    Update: continuation des recherches sur la bdd et l'inscription

--- register.php
+++ register.php
@@ -1,24 +1,41 @@
 <?php session_start();
-require('config/config.php');
-require('model/functions.fn.php');
+include('config/config.php');
+include('model/functions.fn.php');
 
 /*===============================
 	Register
 ===============================*/
 
-if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])){
-	if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
+if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['password']) && !empty($_POST['password']) && isset($_POST['username']) && !empty($_POST['username'])){
+
+		userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
 
 		header('Location: login.php');
 
 	}else{
 		$error = 'Champs requis !';
-	}
+	
 }
 
 // username and email available
-// isUsernameAvailable();
-// isEmailAvailable();
+if(isUsernameAvailable($db, $_POST['username'])){
+
+	header('Location: login.php');
+
+}else{
+
+	$error = 'Username indisponible';
+}
+
+if(isEmailAvailable($db, $_POST['email'])){
+
+	header('Location: login.php');
+
+}else{
+
+	$error = 'Email indisponible';
+	
+}
 
 include 'view/_header.php';
 include 'view/register.php';
